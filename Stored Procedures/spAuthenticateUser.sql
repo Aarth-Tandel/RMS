49e79b03db9a99b1d3322550ab402457bb27578f
Aarth Tandel <aarth.996@gmail.com>
2017-06-10 14:53:24 +0530

    SQL Schema and Procedures

---
+++ Stored Procedures/spAuthenticateUser.sql
@@ -0,0 +1,19 @@
+Create Procedure spAuthenticateUser
+@UserName nvarchar(100),
+@Password nvarchar(100)
+as
+Begin
+ Declare @Count int
+ 
+ Select @Count = COUNT(UserName) from Registration_9227
+ where [UserName] = @UserName and [Password] = @Password
+ 
+ if(@Count = 1)
+ Begin
+  Select 1 as ReturnCode
+ End
+ Else
+ Begin
+  Select -1 as ReturnCode
+ End
+End
